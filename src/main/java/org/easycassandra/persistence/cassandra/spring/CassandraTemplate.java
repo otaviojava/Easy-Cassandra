6c0054c7526c8cfc3416fbfa40e1cac63809e605
nenita <ncasuga@netpulse.com>
2013-10-31 17:41:31 -0700

    Summary of changes:
    - Exposing the ability to query by primary key + index and range to CassandraTemplate so it could be used by Spring apps

--- src/main/java/org/easycassandra/persistence/cassandra/spring/CassandraTemplate.java
+++ src/main/java/org/easycassandra/persistence/cassandra/spring/CassandraTemplate.java
@@ -23,9 +23,9 @@ import com.datastax.driver.core.ConsistencyLevel;
 /**
  * Base to cassandra databases, the interface has all resources that may use in
  * Cassandra database
- * 
+ *
  * @author otaviojava
- * 
+ *
  */
 public interface CassandraTemplate{
 		/**
@@ -41,14 +41,14 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T> T save(T entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * save a list of objects
 	     * @param entities
 	     * @return
 	     */
 	    <T> Iterable<T> save(Iterable<T> entities);
-	    
+
 	    /**
 	     * saves a list of objects with consistency level
 	     * @param entities
@@ -56,40 +56,40 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T> Iterable<T> save(Iterable<T> entities,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * delete an entity
 	     * @param entity
 	     */
 	    <T> void delete(T entity);
-	    
+
 	    /**
 	     * delete an entity with consistence level
 	     * @param entity
 	     * @param consistency
 	     */
 	    <T> void delete(T entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * delete an interable os entities
 	     * @param entities
 	     */
 	    <T> void delete(Iterable<T> entities);
-	    
+
 	    /**
 	     * delete an interable os entities with consistency level
 	     * @param entities
 	     * @param consistency
 	     */
 	    <T> void delete(Iterable<T> entities,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * delete an object from a key
 	     * @param key
 	     * @param entity
 	     */
 	    <K> void delete(K key, Class<?> entity);
-	    
+
 	    /**
 	     * delete an object from a key with consistency level
 	     * @param key
@@ -97,14 +97,14 @@ public interface CassandraTemplate{
 	     * @param consistency
 	     */
 	    <K> void delete(K key, Class<?> entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * delete objects from keys
 	     * @param keys
 	     * @param entity
 	     */
 	    <K,T> void delete(Iterable<K>  keys, Class<T> entity);
-	    
+
 	    /**
 	     * delete objects from keys with consistency level
 	     * @param keys
@@ -118,7 +118,7 @@ public interface CassandraTemplate{
 	     * @param entity
 	     */
 	    <T> void deleteAll(Class<T> entity);
-	    
+
 
 	    /**
 	     * update the object
@@ -126,7 +126,7 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T> T update(T entity);
-	    
+
 	    /**
 	     * update the object
 	     * @param entity
@@ -134,23 +134,23 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T> T update(T entity,ConsistencyLevel consistency);
-	    
-	    
+
+
 	    /**
 	     * update these objects
 	     * @param entities
 	     * @return
 	     */
 	    <T> Iterable<T> update(Iterable<T> entities);
-	    
+
 	    /**
 	     * update these objects with consistency level
 	     * @param entities
 	     * @return
 	     */
 	    <T> Iterable<T> update(Iterable<T> entities,ConsistencyLevel consistency);
-	    
-	    
+
+
 	    /**
 	     * find one entity by key
 	     * @param key
@@ -166,7 +166,7 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T,K> T findOne(K key, Class<T> entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * return all objects
 	     * @param keys
@@ -174,18 +174,18 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T,K> List<T> findAll(Iterable<K> keys, Class<T> entity);
-	    
+
 	    <T,K> List<T> findAll(Iterable<K> keys, Class<T> entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * returns all entities
 	     * @param entity
 	     * @return
 	     */
 	    <T> List<T> findAll(Class<T> entity);
-	    
+
 	    <T> List<T> findAll(Class<T> entity,ConsistencyLevel consistency);
-	    
+
 	    /**
 	     * return by index
 	     * @param index
@@ -193,17 +193,49 @@ public interface CassandraTemplate{
 	     * @return
 	     */
 	    <T,I> List<T> findByIndex(String columnName,I index,Class<T> entity);
-	    
+
 	    <T,I> List<T> findByIndex(String columnName,I index,Class<T> entity,ConsistencyLevel consistency);
-	    
-	    /**
+
+        /**
+        * Find by key and annotated index
+        *
+        * @author Nenita Casuga
+        * @since 10/31/2013
+        */
+        <T,I> List<T> findByKeyAndIndex(String key, I index,Class<T> entity);
+
+        /**
+        * Find by key and annotated index
+        *
+        * @author Nenita Casuga
+        * @since 10/30/2013
+        */
+        <T,I> List<T> findByKeyAndIndex(String key, I index,Class<T> entity,ConsistencyLevel consistency);
+
+        /**
+        * Find by key and annotated index
+        *
+        * @author Nenita Casuga
+        * @since 10/31/2013
+        */
+        <T,I> List<T> findByKeyAndIndexRange(String key, I indexStart, I indexEnd, boolean exclusive,Class<T> entity);
+
+        /**
+        * Find by key and annotated index
+        *
+        * @author Nenita Casuga
+        * @since 10/30/2013
+        */
+        <T,I> List<T> findByKeyAndIndexRange(String key, I indexStart, I indexEnd, boolean exclusive, Class<T> entity,ConsistencyLevel consistency);
+
+        /**
 	     * verify if exist
 	     * @param key
 	     * @param entity
 	     * @return
 	     */
 	    <K,T>boolean exist(K key, Class<T> entity);
-	    
+
 	    <K,T>boolean exist(K key, Class<T> entity,ConsistencyLevel consistency);
 
 	    /**
@@ -211,14 +243,14 @@ public interface CassandraTemplate{
 	     * @param query
 	     */
 	    void executeUpdate(String query);
-	    
+
 	    /**
 	     * counts a number of rows in column family
 	     * @param bean
 	     * @return
 	     */
 	    <T>long count(Class<T> bean);
-	    
+
 	    <T>long count(Class<T> bean,ConsistencyLevel consistency);
-   
+
 }
