4cb2a08ccf49c1316c3fdb50489b8c64a8348e32
otaviojava <otaviojava@java.net>
2014-01-30 22:54:10 -0200

    checkstyle

--- src/test/java/org/easycassandra/bean/dao/PersistenceDao.java
+++ src/test/java/org/easycassandra/bean/dao/PersistenceDao.java
@@ -6,12 +6,22 @@ import org.easycassandra.Constants;
 import org.easycassandra.persistence.cassandra.EasyCassandraManager;
 import org.easycassandra.persistence.cassandra.Persistence;
 
-public class PersistenceDao<T,K> {
+/**
+ * Base to persistence.
+ * @author otaviojava
+ * @param <T> the entity
+ * @param <K> the key
+ */
+public class PersistenceDao<T, K> {
 
     private Persistence persistence;
     private Class<T> baseClass;
-    private static EasyCassandraManager easyCassandraManager = new EasyCassandraManager(Constants.HOST,Constants.KEY_SPACE);
-
+    private static EasyCassandraManager easyCassandraManager = new EasyCassandraManager(
+            Constants.HOST, Constants.KEY_SPACE);
+    /**
+     * the constructor.
+     * @param baseClass the class
+     */
     public PersistenceDao(Class<T> baseClass) {
         this.baseClass = baseClass;
 
@@ -19,64 +29,111 @@ public class PersistenceDao<T,K> {
         easyCassandraManager.addFamilyObject(baseClass, Constants.KEY_SPACE);
 
     }
-
+    /**
+     * insert.
+     * @param bean the bean
+     * @return if was with success
+     */
     public boolean insert(T bean) {
         return persistence.insert(bean);
     }
-
+    /**
+     * remove.
+     * @param bean the bean
+     * @return if was with success
+     */
     public boolean remove(T bean) {
         return persistence.delete(bean);
     }
-
+    /**
+     * removeFromRowKey.
+     * @param rowKey the rowkey
+     * @return if was with success
+     */
     public boolean removeFromRowKey(K rowKey) {
         return persistence.deleteByKey(rowKey, baseClass);
     }
-
+    /**
+     * update.
+     * @param bean the bean
+     * @return the bean
+     */
     public boolean update(T bean) {
         return persistence.update(bean);
     }
-
+    /**
+     * retrieve.
+     * @param id the id
+     * @return the object
+     */
     public T retrieve(Object id) {
-
         return   persistence.findByKey(id, baseClass);
     }
 
+    /**
+     * listAll.
+     * @return list all
+     */
     public List<T> listAll() {
         return persistence.findAll(baseClass);
     }
-
-    public List<T> listByIndex(String indexName,Object index) {
-        return persistence.findByIndex(indexName,index, baseClass);
+    /**
+     * listByIndex.
+     * @param indexName the indexName
+     * @param index the index
+     * @return the list
+     */
+    public List<T> listByIndex(String indexName, Object index) {
+        return persistence.findByIndex(indexName, index, baseClass);
     }
 
     /**
-     * Find by key and annotated index
-     *
+     * Find by key and annotated index.
+     * @param id the id
+     * @param index the index
      * @author Nenita Casuga
      * @since 10/30/2013
+     * @return the list
      */
     public List<T> listByKeyAndIndex(Object id, Object index) {
         return persistence.findByKeyAndIndex(id, index, baseClass);
     }
 
     /**
-     * Find by key and annotated index range
-     *
+     * listByKeyAndIndexRange.
+     * @param id the id
+     * @param indexStart the indexStart
+     * @param indexEnd the indexEnd
+     * @param inclusive the inclusive
      * @author Nenita Casuga
-     * @since 10/30/2013
+     * @since 10/30/201
+     * @return the list
      */
-    public List<T> listByKeyAndIndexRange(Object id, Object indexStart, Object indexEnd, boolean inclusive) {
-        return persistence.findByKeyAndIndexRange(id, indexStart, indexEnd, inclusive, baseClass);
+    public List<T> listByKeyAndIndexRange(Object id, Object indexStart,
+            Object indexEnd, boolean inclusive) {
+        return persistence.findByKeyAndIndexRange(id, indexStart, indexEnd,
+                inclusive, baseClass);
     }
-    
+    /**
+     * execute a query.
+     * @param query the query
+     * @return if was with success
+     */
     public boolean executeUpdate(String query) {
     	return persistence.executeUpdate(query);
     }
-
+    /**
+     * count.
+     * @return the count.
+     */
     public Long count() {
         return persistence.count(baseClass);
     }
-
+    /**
+     * execute the query.
+     * @param string the query
+     * @return if was with success
+     */
     public boolean executeUpdateCql(String string) {
         return persistence.executeUpdate(string);
     }
